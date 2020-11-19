3f12fa778f7c73a0d612fa58510e552d4f7161bc
Konrad Korf <konrad.korf@gmail.com>
2020-11-19 21:17:37 +0100

    reload page test added

--- CreditCards.UITests/CreditCardTests.cs
+++ CreditCards.UITests/CreditCardTests.cs
@@ -1,20 +1,50 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 using Xunit;
 
 namespace CreditCards.UITests
 {
     public class CreditCardTests
     {
+        private const string HomeUrl = "http://localhost:44108/";
+        private const string HomeTitle = "Home Page - Credit Cards";
         [Fact]
-        [Trait("Category","Smoke")]
+        [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("http://localhost:44108/");
+                //Arrange
+
+                //Act
+                driver.Navigate().GoToUrl(HomeUrl);
+                Thread.Sleep(3000);
+
+                //Assert
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
             }
         }
 
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange
+
+                //Act
+                driver.Navigate().GoToUrl(HomeUrl);
+                Thread.Sleep(3000);
+
+                driver.Navigate().Refresh();
+
+                //Assert
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+        }
     }
 }
