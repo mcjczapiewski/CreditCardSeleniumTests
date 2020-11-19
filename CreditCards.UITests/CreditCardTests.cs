1d81db6a8d2d7f523fbe8d7657d72925fa32fdac
Konrad Korf <konrad.korf@gmail.com>
2020-11-19 20:49:30 +0100

    First inital test goes to url

---
+++ CreditCards.UITests/CreditCardTests.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardTests
+    {
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/");
+            }
+        }
+
+    }
+}
