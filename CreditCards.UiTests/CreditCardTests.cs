330c15ad7a6dfb6f96200fbfc1e8e6b1f3565a35
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-11-21 16:26:34 +0100

    UI tests

---
+++ CreditCards.UiTests/CreditCardTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UiTests
+{
+    public class CreditCardTests
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange
+                const string homeUrl = "http://localhost:44108/";
+                const string expectedPageTitle = "Home Page - Credit Cards";
+
+                //Act
+                driver.Navigate().GoToUrl(homeUrl);
+                var pageTitle = driver.Title;
+                Thread.Sleep(5000);
+
+                //Assert
+                Assert.Equal(expectedPageTitle, pageTitle);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange
+                const string homeUrl = "http://localhost:44108/";
+
+                //Act
+                driver.Navigate().GoToUrl(homeUrl);
+                var firstGuid = driver.FindElement(By.Id("GenerationToken")).Text;
+                Thread.Sleep(2000);
+                driver.Navigate().Refresh();
+                var secondGuid = driver.FindElement(By.Id("GenerationToken")).Text;
+                Thread.Sleep(2000);
+
+                //Assert
+                Assert.NotEqual(firstGuid, secondGuid);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange
+                const string homeUrl = "http://localhost:44108/";
+                const string aboutUrl = "http://localhost:44108/Home/About";
+                const string expectedPageTitle = "Home Page - Credit Cards";
+
+                //Act
+                driver.Navigate().GoToUrl(homeUrl);
+                driver.Navigate().GoToUrl(aboutUrl);
+                driver.Navigate().Back();
+                driver.Navigate().Forward();
+                var pageTitle = driver.Title;
+                var pageUrl = driver.Url;
+                Thread.Sleep(2000);
+
+                //Assert
+                Assert.NotEqual(homeUrl, pageUrl);
+                Assert.NotEqual(expectedPageTitle, pageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ClickEasyApplyNow()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange
+                const string homeUrl = "http://localhost:44108/";
+                const string buttonName = "Easy: Apply Now!";
+
+                //Act
+                driver.Navigate().GoToUrl(homeUrl);
+                var buttonRight = driver.FindElement(By.ClassName("glyphicon-chevron-right"));
+                Thread.Sleep(2000);
+                
+                buttonRight.Click();
+                Thread.Sleep(2000);
+
+                var buttonApply = driver.FindElement(By.XPath($"//a[contains(text(),'{buttonName}')]"));
+
+                //Assert
+            }
+        }
+    }
+}
---
+++ CreditCards.UiTests/SeleniumEasyTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UiTests
+{
+    public class SeleniumEasyTests
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void SingleInputField()
+        {
+            var homeUrl = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
+
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange
+                const string inputText = "Stop the count!";
+                const string buttonName = "Show Message";
+
+                //Act
+                driver.Navigate().GoToUrl(homeUrl);
+                Thread.Sleep(1500);
+                
+                var closeAdd = driver.FindElement(By.Id("at-cv-lightbox-close"));
+                closeAdd.Click();
+                Thread.Sleep(2000);
+
+                var inputBox = driver.FindElement(By.Id("user-message"));
+                inputBox.SendKeys(inputText);
+                Thread.Sleep(2000);
+                
+                var showButton = driver.FindElement(By.XPath($"//button[contains(text(),'{buttonName}')]"));
+                showButton.Click();
+
+                var display = driver.FindElement(By.Id("display"));
+
+                //Assert
+                Assert.Equal(inputText, display.Text);
+            }
+        }
+    }
+}
